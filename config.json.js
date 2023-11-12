f92b1107a8ad38a2c94cb6a203972c76cdd39798
PCKIT <30070768+PCKIT@users.noreply.github.com>
2023-11-12 15:00:52 +0800

    Update config.json.js

--- config.json.js
+++ config.json.js
@@ -1,6 +1,6 @@
 {
-"version":"4.1.8",
-"md5":"1c6be57a5e7ddc0e20ef3a6c2e41bd88",
+"version":"4.1.7",
+"md5":"75A1791F7182580B753D417A201DA534",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
